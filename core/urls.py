077e3f880b3d8b0b3b20f88dff662b62749f4f75
Luke Arnold <luke.arnold@Luke-Arnold-C02V90P1HTD6.local>
2024-01-11 12:26:32 -0700

    edited all pages

--- core/urls.py
+++ core/urls.py
@@ -5,4 +5,11 @@ from . import views
 app_name = ''
 urlpatterns = [
 	path('home/', views.index, name='home'),
+	path('contact/', views.contact, name='contact'),
+	path('blog/', views.contact, name='blog'),
+	path('pricing/', views.contact, name='pricing'),
+	path('gallery/', views.contact, name='gallery'),
+	path('about/', views.about, name='about'),
+	path('services', views.services, name='services'),
+	path('',views.index, name='')
 ]
--- core/views.py
+++ core/views.py
@@ -5,4 +5,22 @@ from django.shortcuts import get_object_or_404, render, redirect
 
 def index(request):
     return render(request, 'core/index.html')
-# Create your views here.
+
+def pricing(request):
+    return render(request, 'core/pricing.html')
+
+def gallery(request):
+    return render(request, 'core/gallery.html')
+
+def about(request):
+    return render(request, 'core/about.html')
+
+def contact(request):
+    return render(request, 'core/contact.html')
+
+def about(request):
+    return render(request, 'core/about.html')
+
+def services(request):
+    return render(request, 'core/services.html')
+
